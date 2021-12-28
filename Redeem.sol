9d23e9054ed3685a231d09f2fb61a04d08fb7411
RedeemDev <93149415+RedeemDev@users.noreply.github.com>
2021-12-28 18:49:17 -0500

    Updated SmartContract

--- Redeem.sol
+++ Redeem.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+ 
+// SPDX-License-Identifier: unidentified
 //Polygon MATIC
 
 
@@ -8,8 +9,7 @@ interface IERC20 {
   function totalSupply() external view returns (uint256); 
   function decimals() external view returns (uint8); 
   function symbol() external view returns (string memory); 
-  function name() external view returns (string memory); 
-  function getOwner() external view returns (address); 
+  function name() external view returns (string memory);  
   function balanceOf(address account) external view returns (uint256); 
   function transfer(address recipient, uint256 amount) external returns (bool); 
   function allowance(address _owner, address spender) external view returns (uint256); 
@@ -132,7 +132,7 @@ abstract contract Context {
   constructor ()  { }
 
   function _msgSender() internal view virtual returns  (address payable) {
-    return payable(msg.sender);
+    return payable (msg.sender);
   }
 
   function _msgData() internal view virtual returns  (bytes memory) {
@@ -140,50 +140,86 @@ abstract contract Context {
     return msg.data;
   }
 } 
+
 library SafeMath { 
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    require(c >= a, "SafeMath: addition overflow");
-
-    return c;
-  } 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    return sub(a, b, "SafeMath: subtraction overflow");
-  } 
-  function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-    require(b <= a, errorMessage);
-    uint256 c = a - b;
-
-    return c;
-  } 
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) { 
-    if (a == 0) {
-      return 0;
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    } 
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    } 
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked { 
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    } 
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    } 
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    } 
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    } 
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    } 
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    } 
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    } 
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    } 
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    } 
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    } 
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
     }
-
-    uint256 c = a * b;
-    require(c / a == b, "SafeMath: multiplication overflow");
-
-    return c;
-  }
- 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    return div(a, b, "SafeMath: division by zero");
-  } 
-  function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) { 
-    require(b > 0, errorMessage);
-    uint256 c = a / b;  
-    return c;
-  }
- 
-  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-    return mod(a, b, "SafeMath: modulo by zero");
-  }
- 
-  function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-    require(b != 0, errorMessage);
-    return a % b;
-  }
 }
  
 contract Ownable is Context {
@@ -195,97 +231,146 @@ contract Ownable is Context {
     address msgSender = _msgSender();
     _owner = msgSender;
     emit OwnershipTransferred(address(0), msgSender);
-  } 
+  }
+
   function owner() public view returns (address) {
     return _owner;
-  }
- 
+  } 
   modifier onlyOwner() {
     require(_owner == _msgSender(), "Ownable: caller is not the owner");
     _;
-  } 
+  }
+
   function renounceOwnership() public onlyOwner {
     emit OwnershipTransferred(_owner, address(0));
     _owner = address(0);
-  } 
+  }
+
   function transferOwnership(address newOwner) public onlyOwner {
     _transferOwnership(newOwner);
-  } 
+  }
+
   function _transferOwnership(address newOwner) internal {
     require(newOwner != address(0), "Ownable: new owner is the zero address");
     emit OwnershipTransferred(_owner, newOwner);
     _owner = newOwner;
   }
+ 
 }
 
-contract Redeem is Context, IERC20, Ownable {
+  contract  Redeem  is  Context, IERC20, Ownable  {
   using SafeMath for uint256;
+  using Address for address;
 
-  mapping (address => uint256) private _balances;
+  mapping (address => uint256) private _balances;  
   mapping (address => mapping (address => uint256)) private _allowances;
+  mapping (address => bool) private _excludedFromFee; 
 
-  uint256 private _totalSupply = 150 *10**6 * 10**8;
-  uint8 public _decimals = 8;
+  uint256 private _totalSupply = 150 *10**6;
+  uint8 public _decimals = 0;
   string public _symbol = "RDM";
-  string public _name = "Redeem";
+  string public _name = "Redeem"; 
+
+  uint256 public _liquidityFee = 2;
+  uint256 private _previousLiquidityFee = _liquidityFee;
   
   IUniswapV2Router02 public immutable uniswapV2Router;
   address public immutable uniswapV2Pair;
+
+  bool swappingForLiquidity;
+  bool public swapAddLiquidityAllowed = true; 
+     
+  uint256 private redeemReqToAddLiquidity = 5 * 10**6;
+  
+  event TokensForLiquidity(uint256 tokens);
+  event MinTokensBeforeSwapUpdated(uint256 redeemReqToAddLiquidity); 
+  event SwapAndLiquifyEnabledUpdated(bool enabled);
+  event SwapAndLiquify(
+        uint256 tokensSwapped,
+        uint256 ethReceived,
+        uint256 tokensIntoLiqudity
+  );
+  //Determines the state of the contracts liquiditySwap feature. 
+  modifier preventMultSwaps {
+        swappingForLiquidity = true;
+        _;
+        swappingForLiquidity = false;
+  }
 
   constructor() { 
 
     _balances[msg.sender] = _totalSupply;
 
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff);
-        //0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff <-- Quickswap Router (Main) 
+        //0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff <-- Quickswap Router (Main)   
          // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
         .createPair(address(this), _uniswapV2Router.WETH());
 
         // set the rest of the contract variables
         uniswapV2Router = _uniswapV2Router;
-
+        //Exclude Owner and this address from _liquidityFee
+        _excludedFromFee[owner()] = true;
+        _excludedFromFee[address(this)] = true; 
     emit Transfer(address(0), msg.sender, _totalSupply);
 
-  }
+  } 
  
-  function getOwner() override external view returns (address) {
-    return owner();
-  }
- 
-  function decimals() override external view returns (uint8) {
+  function decimals() override public view returns (uint8) {
     return _decimals;
   }
  
-  function symbol() override external view returns (string memory) {
+  function symbol() override public view returns (string memory) {
     return _symbol;
   }
  
-  function name() override external view returns (string memory) {
+  function name() override public view returns (string memory) {
     return _name;
   }
  
-  function totalSupply() override external view returns (uint256) {
+  function totalSupply() override public view returns (uint256) {
     return _totalSupply;
   } 
 
-  function balanceOf(address account) override external view returns (uint256) {
-    return _balances[account];
+  function balanceOf(address account)  public view override returns (uint256) { 
+      return _balances[account]; 
   }
  
-  function transfer(address recipient, uint256 amount) override external returns (bool) {
+  function transfer(address recipient, uint256 amount) override public returns (bool) {
     _transfer(_msgSender(), recipient, amount);
     return true;
   }
+
+  // Only Owner functions /////////////////////////////////////////////////////////////////////
+
+
+
+  function excludeAddressFromFee(address account) public onlyOwner() { 
+        require(!_excludedFromFee[account], "Account is already excluded");  
+        _excludedFromFee[account] = true;
+  }
  
-  function allowance(address owner, address spender) override external view returns (uint256) {
+  function setLiquidityFee(uint256 liquidityFee) public onlyOwner() {
+        _liquidityFee = liquidityFee;
+  }
+
+  function setSwapAndLiquifyEnabled(bool _state) public onlyOwner() {
+        swapAddLiquidityAllowed = _state;
+        emit SwapAndLiquifyEnabledUpdated(_state);
+  } 
+
+  // Only Owner functions /////////////////////////////////////////////////////////////////////
+ 
+  function allowance(address owner, address spender) override public view returns (uint256) {
     return _allowances[owner][spender];
   } 
-  function approve(address spender, uint256 amount) override external returns (bool) {
+  
+  function approve(address spender, uint256 amount) override public returns (bool) {
     _approve(_msgSender(), spender, amount);
     return true;
   } 
-  function transferFrom(address sender, address recipient, uint256 amount) override external returns (bool) {
+
+  function transferFrom(address sender, address recipient, uint256 amount) override public returns (bool) {
     _transfer(sender, recipient, amount);
     _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
     return true;
@@ -305,20 +390,6 @@ contract Redeem is Context, IERC20, Ownable {
     _burn(_msgSender(), amount);
     return true;
   }
- 
-  function _transfer(
-    address sender, 
-    address recipient, 
-    uint256 amount
-    ) private {
-    require(sender != address(0), "ERC20: transfer from the zero address");
-    require(recipient != address(0), "ERC20: transfer to the zero address");
-
-    _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
-    _balances[recipient] = _balances[recipient].add(amount);
-    emit Transfer(sender, recipient, amount);
-  }
-  
   function _burn(address account, uint256 amount) internal {
     require(account != address(0), "ERC20: burn from the zero address");
 
@@ -339,6 +410,156 @@ contract Redeem is Context, IERC20, Ownable {
     _burn(account, amount);
     _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "ERC20: burn amount exceeds allowance"));
   }
+  //to recieve from uniswapV2Router when swapping
+  receive() external payable {}
+
+  
+  function _transfer(
+    address sender, 
+    address recipient, 
+    uint256 amount
+    ) private {
+    require(sender != address(0), "ERC20: transfer from the zero address");
+    require(recipient != address(0), "ERC20: transfer to the zero address");
+    require(amount > 0, "Transfer amount must be greater than zero");
+
+    uint256 redeemContractBalance = balanceOf(address(this)); 
+
+    bool readyToAddLiquidity = redeemContractBalance >= redeemReqToAddLiquidity;
+
+    if (
+            readyToAddLiquidity &&
+            !swappingForLiquidity &&
+            sender != uniswapV2Pair &&
+            swapAddLiquidityAllowed
+        ) {
+            redeemContractBalance = redeemReqToAddLiquidity;
+            //add liquidity with RedeemReqToAddLiquidity
+            swapAndLiquify(redeemContractBalance);
+        }
+        //begin by establishing need to deduct fee from transfer
+        bool deductLiquidityFee = true; 
+        //if either account belongs to _excludedFromFee account then remove the fee
+        if(_excludedFromFee[sender] || _excludedFromFee[recipient]){
+            deductLiquidityFee = false;
+        }
+         
+      //transfer, it will take liquidity fee
+        _continueTransfer(sender, recipient, amount, deductLiquidityFee);
+  }
+
+  function _continueTransfer(address sender, address recipient, uint256 amount, bool deductLiquidityFee) private {
+        if(!deductLiquidityFee){
+          zeroLiquidityFee();
+        } 
+
+        if(deductLiquidityFee){
+           _transferWithFee(sender,recipient,amount);
+        } else {
+          _transferWithoutFee(sender,recipient,amount);
+        } 
+
+        if(!deductLiquidityFee){
+        reinstateLiquidityFee();
+        }
+            
+  }
+
+  function _transferWithFee(address sender, address recipient, uint256 amount) private {
+ 
+         uint256 redeemForLiquidity = calcTokensForLiquidity(amount);
+         uint256 remainingRedeem = amount.sub(redeemForLiquidity, "Error occured at remainingRedeem sub"); 
+         
+        _balances[sender] = _balances[sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(remainingRedeem);
+ 
+        _takeLiquidityTokens(redeemForLiquidity);
+        emit Transfer(sender, recipient, remainingRedeem);
+  }
+
+  function _transferWithoutFee(address sender, address recipient, uint256 amount) private  { 
+      
+
+        _balances[sender] = _balances[sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount); 
+        emit Transfer(sender, recipient,  amount);
+  }
+
+  function _takeLiquidityTokens(uint256 tokens) private {
+
+        _balances[address(this)].add(tokens); 
+        emit TokensForLiquidity(tokens); 
+  }
+
+
+  function calcTokensForLiquidity(uint256 amount) private view returns (uint256) {
+        return amount.mul(_liquidityFee).div(10**2);
+  } 
+
+  function zeroLiquidityFee() private {
+        if(_liquidityFee == 0) return;
+        _previousLiquidityFee = _liquidityFee;
+        _liquidityFee = 0;
+  }
+    
+  function reinstateLiquidityFee() private {
+        _liquidityFee = _previousLiquidityFee;
+  }
+ 
+  function swapAndLiquify(uint256 redeemContractBalance) private preventMultSwaps {
+        // split the contract balance into halves
+        uint256 half = redeemContractBalance.div(2);
+        uint256 otherHalf = redeemContractBalance.sub(half);
+
+        // capture redeem contracts current balance. 
+        uint256 initialBalance = address(this).balance;
+
+        // swap tokens for ETH
+        swapTokensForEth(half); //  
+
+        // how much ETH did we just swap into?
+        uint256 newBalance = address(this).balance.sub(initialBalance);
+
+        // add liquidity to uniswap
+        addLiquidity(otherHalf, newBalance);
+        
+        emit SwapAndLiquify(half, newBalance, otherHalf);
+    }
+
+    function swapTokensForEth(uint256 tokenAmount) private {
+        // generate the uniswap pair path of token -> weth
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = uniswapV2Router.WETH();
+        // approve token transfer to cover all possible scenarios
+        _approve(address(this), address(uniswapV2Router), tokenAmount);
+
+        // make the swap
+        uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            tokenAmount,
+            0, // accept any amount of ETH
+            path,
+            address(this),
+            block.timestamp
+        );
+    }
+
+    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
+        // approve token transfer to cover all possible scenarios
+        _approve(address(this), address(uniswapV2Router), tokenAmount);
+
+        // add the liquidity
+        uniswapV2Router.addLiquidityETH{value: ethAmount}(
+            address(this),
+            tokenAmount,
+            0, // slippage is unavoidable
+            0, // slippage is unavoidable
+            owner(),
+            block.timestamp
+        );
+    }
+  
+  
 }
  
 
